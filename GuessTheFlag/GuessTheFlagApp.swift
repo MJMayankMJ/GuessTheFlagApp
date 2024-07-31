712b6d7665e3cdf37f9d8f3e6897187e75d0c700
MJMayankMJ <226301131@gkv.ac.in>
2024-07-31 14:45:15 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mayank Jangid on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
